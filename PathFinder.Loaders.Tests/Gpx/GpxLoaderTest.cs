64be41967f02afb44c3546bcf6d0f72bac6ef100
Alex Ullrich <alex.ullrich@gmail.com>
2012-08-31 16:44:38 -0400

    check that waypoints are actually populated

--- PathFinder.Loaders.Tests/Gpx/GpxLoaderTest.cs
+++ PathFinder.Loaders.Tests/Gpx/GpxLoaderTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
 using PathFinder.Loaders.Gpx;
 
 namespace PathFinder.Loaders.Tests.Gpx
@@ -8,13 +10,14 @@ namespace PathFinder.Loaders.Tests.Gpx
     {
         [TestCase("SampleFiles\\DamLoop.gpx", 1, 1155)]
         [TestCase("SampleFiles\\GreenLane.gpx", 1, 1665)]//with garmin extensions
-        public void Load(string filePath, int trackCount, int trackpointCount)
+        public void Load(string filePath, int wayCount, int waypointCount)
         {
             var loader = new GpxFileLoader();
 
             var gpsData = loader.Load(filePath);
-            Assert.AreEqual(trackCount, gpsData.Count);
-            Assert.AreEqual(trackpointCount, gpsData[0].Count);
+            Assert.AreEqual(wayCount, gpsData.Count);
+            Assert.AreEqual(waypointCount, gpsData[0].Count);
+            Assert.True(gpsData[0].All(wp => Math.Abs(wp.Latitude) > 0 && Math.Abs(wp.Longitude) > 0 && wp.Elevation > 0));
         }
     }
 }
