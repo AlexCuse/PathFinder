cd6c0b1304d8dbfc7f50cef61af12b9d53968828
Alex Ullrich <alex.ullrich@gmail.com>
2013-02-25 20:13:24 -0500

    unit tests for data collectors

--- PathFinder.Tests/Domain/Distance/OdometerTest.cs
+++ PathFinder.Tests/Domain/Distance/OdometerTest.cs
@@ -10,10 +10,37 @@ namespace PathFinder.Tests.Domain.Distance
     [TestFixture]
     public class OdometerTest
     {
+        [Test]
+        public void Collect()
+        {
+            var philadelphia = new WayPoint
+            {
+                Latitude = 39.9522,
+                Longitude = 75.1642
+            };
+
+            var sanFrancisco = new WayPoint
+            {
+                Latitude = 37.7750,
+                Longitude = 122.4183
+            };
+
+            var expectedDistance = 4050385.9129165481;
+
+            var odometer = new Odometer();
+            var reverseOdometer = new Odometer();
+
+            odometer.Collect(philadelphia, sanFrancisco);
+            reverseOdometer.Collect(sanFrancisco, philadelphia);
+
+            Assert.AreEqual(expectedDistance, odometer.Distance);
+            Assert.AreEqual(expectedDistance, reverseOdometer.Distance);
+        }
+
         [TestCase("GreenLane.gpx", 14385.166705079719)]
         [TestCase("DamLoop.gpx", 93679.018730527605)]
         [TestCase("WhiteClay.gpx", 36112.637640450499)]
-        public void Time(string fileName, double distanceInMeters)
+        public void Collect_Integration(string fileName, double distanceInMeters)
         {
             var data = LoadData(fileName);
 
--- PathFinder.Tests/Domain/Timing/TimerTest.cs
+++ PathFinder.Tests/Domain/Timing/TimerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using NUnit.Framework;
 using PathFinder.Domain;
@@ -10,16 +11,76 @@ namespace PathFinder.Tests.Domain.Timing
     [TestFixture]
     public class TimerTest
     {
+        //.44074 meters/second = approximately 1 MPH 
+        //this gets us very close to garmin's moving time calculation
+        private double _velocityThresholdForMovingTime = .44704;
+
+        [Test]
+        public void Collect()
+        {
+            var start = DateTime.Now;
+            var end = start.AddDays(3);
+
+            var philadelphia = new WayPoint
+            {
+                Latitude = 39.9522,
+                Longitude = 75.1642,
+                TimeStamp = start
+            };
+
+            var sanFrancisco = new WayPoint
+            {
+                Latitude = 37.7750,
+                Longitude = 122.4183,
+                TimeStamp = end
+            };
+
+            var timer = new Timer(_velocityThresholdForMovingTime);
+
+            timer.Collect(philadelphia, sanFrancisco);
+
+            Assert.AreEqual((end - start).TotalSeconds, timer.TotalTime);
+            Assert.AreEqual((end - start).TotalSeconds, timer.MovingTime);
+            Assert.AreEqual(0, timer.StoppedTime);
+        }
+
+        [Test]
+        public void Collect_Stopped()
+        {
+            var start = DateTime.Now;
+            var end = start.AddDays(3000);
+
+            var philadelphia = new WayPoint
+            {
+                Latitude = 39.9522,
+                Longitude = 75.1642,
+                TimeStamp = start
+            };
+
+            var sanFrancisco = new WayPoint
+            {
+                Latitude = 37.7750,
+                Longitude = 122.4183,
+                TimeStamp = end
+            };
+
+            var timer = new Timer(_velocityThresholdForMovingTime);
+
+            timer.Collect(philadelphia, sanFrancisco);
+
+            Assert.AreEqual((end - start).TotalSeconds, timer.TotalTime);
+            Assert.AreEqual(0, timer.MovingTime);
+            Assert.AreEqual((end - start).TotalSeconds, timer.StoppedTime);
+        }
+
         [TestCase("GreenLane.gpx", 4683, 4092)]
         [TestCase("DamLoop.gpx", 13246, 12467)]
         [TestCase("WhiteClay.gpx", 12345, 9923)]
-        public void Time(string fileName, int totalTime, int movingTime)
+        public void Collect_Integration(string fileName, int totalTime, int movingTime)
         {
             var data = LoadData(fileName);
 
-            //.44074 meters/second = approximately 1 MPH 
-            //this gets us very close to garmin's moving time calculation
-            var time = new Timer(.44704);
+            var time = new Timer(_velocityThresholdForMovingTime);
 
             var aggregator = new Aggregator(new[] { time });
             aggregator.Aggregate(data);
